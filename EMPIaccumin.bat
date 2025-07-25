feb6bfb19c808e3fcd28c28b508583cd65842d41
Luis <172538795+Luismarav@users.noreply.github.com>
2025-07-25 14:39:50 -0700

    EMPIaccumin.bat
    Emiprx accum in

---
+++ EMPIaccumin.bat
@@ -0,0 +1,42 @@
+#EMPIRX
+
+@echo off
+setlocal enabledelayedexpansion
+
+REM Set PATH variable
+SET PATH=M:\eci\dbc13;%path%
+
+REM Process all text files in the current directory
+for %%F in (*.txt) do (
+    REM Extract the numeric date (YYYYMMDD) from the filename
+    for /f "tokens=4 delims=_" %%A in ("%%F") do (
+        set "date_part=%%A"
+        call set "date_part=%%date_part:~0,8%%"
+    )
+
+    REM Extract month and day from the date
+    call set "month_day=%%date_part:~4,2%%%%date_part:~6,2%%"
+
+    REM Create the new filename in MMDD format
+    call set "new_filename=%%month_day%%.txt"
+
+    REM Check for duplicate files and append a counter if necessary
+    set /a counter=0
+    if exist "!new_filename!" (
+        set /a counter+=1
+        set "new_filename=!month_day!_!counter!.txt"
+    )
+
+    REM Rename the file
+    if exist "%%F" (
+        call rename "%%F" "!new_filename!"
+        echo Renamed %%F to !new_filename!
+    ) else (
+        echo The file %%F does not exist.
+    )
+
+    REM Execute the "build" command for the renamed file
+    build !new_filename! 0731x.txt -t -a
+)
+
+pause
